a16af3770a260004286d91493c63c8ead9572182
Isaiah Norton <isaiah.norton@gmail.com>
2021-06-25 08:53:14 -0400

    Set FEEDSTOCK_ROOT for 0.9.1 to ignore requirements

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,7 @@
 set -e
 set -x
 
+# 0.9.1 patch: set variable expected in setup.py for requirements override
+export FEEDSTOCK_ROOT=1
+
 $PYTHON setup.py install --tiledb="$PREFIX" --single-version-externally-managed --record record.txt
