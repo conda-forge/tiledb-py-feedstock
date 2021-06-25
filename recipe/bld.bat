866a21196c5eb552eab67f1398b97eec6d2446b3
Isaiah Norton <isaiah.norton@gmail.com>
2021-06-25 08:53:14 -0400

    Set FEEDSTOCK_ROOT for 0.9.1 to ignore requirements

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,6 @@
+REM 0.9.1 patch: set variable expected in setup.py for requirements override
+set FEEDSTOCK_ROOT=1
+
 "%PYTHON%" setup.py build_ext --tiledb=%LIBRARY_PREFIX% ^
            install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
