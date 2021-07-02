fee4b872efd4fbd3b493faba7dd896ad64364f6f
Isaiah Norton <isaiah.norton@gmail.com>
2021-07-02 11:34:25 -0400

    Export TILEDB_CONDA_BUILD for setup.py overrides

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,4 @@
-REM 0.9.1 patch: set variable expected in setup.py for requirements override
-set FEEDSTOCK_ROOT=1
+set TILEDB_CONDA_BUILD=1
 
 "%PYTHON%" setup.py build_ext --tiledb=%LIBRARY_PREFIX% ^
            install --single-version-externally-managed --record=record.txt
