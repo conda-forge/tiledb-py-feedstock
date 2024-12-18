a027f7cc9ff275fd72bcb87c73b8099fb891261b
John Blischak <jdblischak@gmail.com>
2024-12-18 18:35:59 -0500

    [ci skip] Only upload binaries from main

--- .scripts/run_win_build.bat
+++ .scripts/run_win_build.bat
@@ -105,6 +105,8 @@ if /i "%CI%" == "azure" (
     )
     set "TEMP=%UPLOAD_TEMP%"
 )
+set "UPLOAD_ON_BRANCH=main"
+:: Note, this needs GIT_BRANCH too
 
 :: Validate
 call :start_group "Validating outputs"
